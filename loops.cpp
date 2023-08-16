bf2c10e98d52b45faf0efc70046b6a5a649c14fd
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-16 09:00:36 +0530

    added_conditional

---
+++ loops.cpp
@@ -0,0 +1,16 @@
+//  All about Loops
+#include<iostream>
+using namespace std;
+        int main(){
+            // while loop
+            int i = 0;
+            while (i<=34)
+            {
+                cout<<"we are number"<<endl<<i;
+                i = i+1;
+            }
+            
+
+
+
+        }
