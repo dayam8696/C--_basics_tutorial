2df1014e54f544c5275cc74fe2ef4daf6b6c573e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-16 09:00:59 +0530

    added_conditional

---
+++ if-else-statement.cpp
@@ -0,0 +1,17 @@
+
+// If -else-statement
+#include<iostream>
+using namespace std;
+        int main(){
+        int age;
+        cout<<"Enter your age "<<endl;
+        cin>>age;
+        if(age>=18){
+                cout<<"you are eligible to vote";
+        }
+           else
+           {
+            cout<< "You are not eligible for voting ";
+           }
+        }
+                   
