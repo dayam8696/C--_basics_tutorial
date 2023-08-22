a413ad391a825dc5fe074c24cf9d9592935f4308
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-08-22 13:13:53 +0530

    nesting of member funciton

---
+++ nesting_member_fun.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class binary{
+    string s ;
+    public:
+    void read (void);
+    void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+    };
+
+void binary ::read(void){
+    cout<<"Enter the Binary number : "; 
+    cin>>s;}
+
+    void binary :: chk_bin(void){
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.at(i) != '0' && s.at(i) != '1'){
+                cout<<"Incorrect binary number"<<endl;
+                exit(0);
+            }
+        }
+        
+    }
+    void binary::ones_compliment(void){
+        chk_bin();
+        cout<<"ones compliment of the entred binary number is "<<endl;
+        for (int  i = 0; i < s.length(); i++)
+        {
+            if (s.at(i)== '0')
+            {
+                s.at(i) = '1';
+            }
+            else{
+                s.at(i)= '0';
+            }
+        }
+        
+    }
+    void binary::display(void){
+        for (int  i = 0; i < s.length(); i++)
+        {
+            cout<< s.at(i);
+        }
+        cout<<endl;
+    }
+
+    int main(){
+        binary b;
+        b.read();
+        //b.chk_bin(); nested we use this function in ones_complement
+        b.ones_compliment();
+        b.display();
+    }
